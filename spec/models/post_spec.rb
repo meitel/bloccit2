9b79147f7ec051ff99a202a8f83fe23ac11ecb09
Melanie Eitel <me@melanieeitel.com>
2015-10-06 21:21:54 -0400

    wip

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
-include RandomData
+# require 'RandomData'
 
 RSpec.describe Post, type: :model do
   
    # #3
-   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
- # #4
-   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+   let(:topic) { Topic.create!(name: "aaaaaa", description: "aaaaaa") }
+   let(:post) { topic.posts.create!(title: "aaaaaa", body: "aaaaaa") }
  
    it { should belong_to(:topic) }
+
+   it { should validate_presence_of(:title) }
+   it { should validate_presence_of(:body) }
+   it { should validate_presence_of(:topic) }
+   it { should validate_length_of(:title).is_at_least(5) }
+   it { should validate_length_of(:body).is_at_least(20) }
  
    describe "attributes" do
  # #2
