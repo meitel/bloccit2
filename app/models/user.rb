de615c287c593429031fd098e173af66125907b6
Melanie Eitel <me@melanieeitel.com>
2015-10-09 15:33:42 -0400

    Assignment 39 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
    # #2
    before_save { self.email = email.downcase }
+   before_save { self.name = self.name.split.map!{|n| n.capitalize}.join(' ') if self.name }
 
  # #3
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   NAME_REGEX = /\A[A-Z]\w+(\s[A-Z]\w+)?\z/
 
  # #4
-  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
+  validates :name, length: { minimum: 1, maximum: 100 }, format: { with: NAME_REGEX }, presence: true
  # #5
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
@@ -19,4 +21,6 @@ class User < ActiveRecord::Base
 
  # #7
    has_secure_password
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User, type: :model do
    # Shoulda tests for name
    it { should validate_presence_of(:name) }
    it { should validate_length_of(:name).is_at_least(1) }
+   it { should allow_value('Bloccit User').for(:name) }
+   it { should_not allow_value('bloccit user').for(:name) }
  
    # Shoulda tests for email
    it { should validate_presence_of(:email) }
@@ -28,12 +30,17 @@ RSpec.describe User, type: :model do
      end
    end
    describe "invalid user" do
-     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
+     let(:user_with_invalid_name) { User.new(name: " ", email: "user@bloccit.com") }
+     let(:user_with_invalid_name_format) { User.new name: 'bloccit user', email: 'user@bloccit.com', password: 'password' }
      let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
      let(:user_with_invalid_email_format) { User.new(name: "Bloccit User", email: "invalid_format") }
  
      it "should be an invalid user due to blank name" do
        expect(user_with_invalid_name).to_not be_valid
+     end
+
+     it 'should be an invalid user due to incorrectly formatted name' do
+      expect(user_with_invalid_name_format).to_not be_valid
      end
  
      it "should be an invalid user due to blank email" do
