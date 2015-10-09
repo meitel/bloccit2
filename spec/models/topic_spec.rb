e7f718862e4c47f2eebf81af3c1ffbe496e3e6a6
Melanie Eitel <me@melanieeitel.com>
2015-10-09 08:13:34 -0400

    Assignment 38 completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe Topic, type: :model do
-     let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
+    it { should validate_length_of(:name).is_at_least(5) }
+    it { should validate_length_of(:description).is_at_least(15) }
  
    describe "attributes" do
      it "should respond to name" do
