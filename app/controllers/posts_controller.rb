1ac5a0715400cb4a081c2363fd1b761a483d06cd
Melanie Eitel <me@melanieeitel.com>
2015-10-16 15:27:49 -0400

    partial completion of assignment 42, labels

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   
   before_action :require_sign_in, except: :show
   before_action :authorize_user, except: [:show, :new, :create]
-   def show
+  def show
     @post = Post.find(params[:id])
   end
 
@@ -38,6 +38,7 @@ def update
  
      if @post.save
        @post.labels = Label.update_labels(params[:post][:labels])
+       @post.rating =  Rating.update_rating(params[:post][:labels])
        flash[:notice] = "Post was updated."
        redirect_to [@post.topic, @post]
      else
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,12 @@
 class Topic < ActiveRecord::Base
+  
+  belongs_to :rating, polymorphic: true
   has_many :posts, dependent: :destroy
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
+
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
+  validates :public, presence: true
 end
