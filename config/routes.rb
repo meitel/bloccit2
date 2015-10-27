90d31bafc20b8dd8543f428d66dc5886e112b5c8
Melanie Eitel <me@melanieeitel.com>
2015-10-27 08:54:43 -0400

    Checkpoint 44 completed, favorites

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ resources :labels, only: [:show]
   
  # #4
    resources :posts, only: [] do
- # #5
-     resources :comments, only: [:create, :destroy]
+   resources :comments, only: [:create, :destroy]
+
      post '/up-vote' => 'votes#up_vote', as: :up_vote
      post '/down-vote' => 'votes#down_vote', as: :down_vote
-
+   resources :favorites, only: [:create, :destroy]
    end
    
  resources :users, only: [:new, :create]
